81cc0324848e2711e1e9084e5b9118db6995f04d
const.nawata <const.nawata@gmail.com>
2019-08-26 22:11:36 +0300

    Exchange entity

---
+++ src/Entity/Exchange.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ExchangeRepository")
+ */
+class Exchange
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    private $symbol;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $ratio;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSymbol(): ?string
+    {
+        return $this->symbol;
+    }
+
+    public function setSymbol(string $symbol): self
+    {
+        $this->symbol = $symbol;
+
+        return $this;
+    }
+
+    public function getRatio(): ?float
+    {
+        return $this->ratio;
+    }
+
+    public function setRatio(float $ratio): self
+    {
+        $this->ratio = $ratio;
+
+        return $this;
+    }
+}
